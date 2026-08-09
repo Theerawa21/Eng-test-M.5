1f4df484b910020dc3986b416d74976ad73129f7
Theerawa21 <numpanoof@gmail.com>
2026-08-09 18:21:33 +0700

    Connect Oxford lookup page to Apps Script API

--- oxford-login/config.js
+++ oxford-login/config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-  apiUrl: 'PASTE_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE'
+  apiUrl: 'https://script.google.com/macros/s/AKfycbzBDC87Up3_Eeg3alIV1hgEuosMiek6WW-FuFYBPEpmn2zNT12X8EzgLZPXIJvM_00twQ/exec'
 };
